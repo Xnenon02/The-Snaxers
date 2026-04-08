f649615d1d1e2f5dad5c49b9ffb14343c00acdf9
Hanita Melin <hanita.2010@live.se>
2026-04-08 16:03:22 +0200

    Add ApplicationDbContext with Favorites DbSet

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TheSnaxers.Models;
+
+namespace TheSnaxers.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Favorite> Favorites { get; set; }
+}
