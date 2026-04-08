eab85a1c674658a3de6d1ff370bd405e4c9175e9
Hanita Melin <hanita.2010@live.se>
2026-04-08 17:39:20 +0200

    Add FavoriteController with add/remove/index actions

---
+++ Controllers/FavoriteController.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheSnaxers.Data;
+using TheSnaxers.Models;
+
+namespace TheSnaxers.Controllers;
+
+[Authorize]
+public class FavoriteController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public FavoriteController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+    {
+        _db = db;
+        _userManager = userManager;
+    }
+
+    // Visa favoriter
+    public async Task<IActionResult> Index()
+    {
+        var userId = _userManager.GetUserId(User);
+
+        var favorites = await _db.Favorites
+            .Include(f => f.Product)
+            .Where(f => f.UserId == userId)
+            .ToListAsync();
+
+        return View(favorites);
+    }
+
+    // Lägg till favorit
+    [HttpPost]
+    public async Task<IActionResult> Add(int productId)
+    {
+        var userId = _userManager.GetUserId(User);
+
+        var exists = await _db.Favorites
+            .AnyAsync(f => f.UserId == userId && f.ProductId == productId);
+
+        if (!exists)
+        {
+            _db.Favorites.Add(new Favorite
+            {
+                UserId = userId!,
+                ProductId = productId
+            });
+
+            await _db.SaveChangesAsync();
+        }
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    // Ta bort favorit
+    [HttpPost]
+    public async Task<IActionResult> Remove(int productId)
+    {
+        var userId = _userManager.GetUserId(User);
+
+        var favorite = await _db.Favorites
+            .FirstOrDefaultAsync(f => f.UserId == userId && f.ProductId == productId);
+
+        if (favorite != null)
+        {
+            _db.Favorites.Remove(favorite);
+            await _db.SaveChangesAsync();
+        }
+
+        return RedirectToAction("Index");
+    }
+}
